ea9c8fc984a268274c8bfcc7d92bcdaade9a1ea8
jayariah77-code <jayariah77@gmail.com>
2026-03-29 23:33:15 +0300

    Add library/menulist/toolsmenu.js

---
+++ library/menulist/toolsmenu.js
@@ -0,0 +1,62 @@
+const toolsMenu = `
+  ╔═══〔 ⚡ QUICK TOOLS 〕═══╗
+
+  ║ .totalfitur
+  ║ .prefix
+  ║ .pair
+  ║ .save
+  ║ .ping
+  ║ .runtime
+  ║ .getcmd
+
+  ╚═══════════════════════╝
+
+  ╔══〔 🛠️ UTILITY TOOLS 〕══╗
+
+  ║ .weather
+  ║ .translate
+  ║ .tts
+  ║ .tinyurl
+  ║ .shorturl
+  ║ .removebg
+  ║ .qr
+  ║ .readqr
+  ║ .xmascard
+  ║ .robottext
+  ║ .webcopier
+
+  ╚═══════════════════════╝
+
+  ╔══〔 🎵 AUDIO & VOICE 〕══╗
+
+  ║ .tomp3
+  ║ .toaudio
+  ║ .toppt
+  ║ .transcript
+  ║ .shazam
+
+  ╚═══════════════════════╝
+
+  ╔══〔 🤖 AI CREATION 〕═══╗
+
+  ║ .fluximg
+  ║ .deepimg
+  ║ .songgenerator →  [prompt]
+
+  ╚═══════════════════════╝
+
+  ╔══〔 👻 PRESENCE TOOLS 〕══╗
+
+  ║ .vv
+
+  ╚═══════════════════════╝
+
+  ╔══〔 📊 STATUS TOOLS 〕══╗
+
+  ║ .autostatus
+  ║ .togroupstatus
+
+  ╚═══════════════════════╝
+  `
+
+  module.exports = toolsMenu
