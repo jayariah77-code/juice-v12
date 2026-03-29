32109e05c835ed4342343be5f72e06377347d860
jayariah77-code <jayariah77@gmail.com>
2026-03-29 23:33:06 +0300

    Add library/menulist/aimenu.js

---
+++ library/menulist/aimenu.js
@@ -0,0 +1,73 @@
+const aiMenu = `
+╔══〔 🤖 AI CHAT ENGINES 〕══╗
+
+║ .ai
+║ .chatgpt
+║ .gpt4
+║ .claude
+║ .gpt-4o
+║ .gpt41-mini
+║ .gptturbo
+║ .gptlogic
+║ .openai
+║ .claudeai
+║ .gemini-ai
+║ .gemini-pro
+║ .deepseek
+║ .metaai
+║ .copilot
+║ .mistral
+║ .grok
+║ .perplexity
+║ .wormgpt
+║ .birdai
+║ .veniceai
+║ .conciseai
+║ .llama-ai
+║ .lumin-ai
+║ .typli-ai
+║ .poly-ai
+║ .quantum-ai
+║ .chatai
+║ .chatevery-where
+║ .chatbotai
+║ .aoyoai
+║ .blackbox-pro
+║ .feloai
+║ .aliceai
+║ .gemmaai
+║ .velynai
+║ .muslimai
+║ .zerogpt
+║ .writecream
+║ .yupraai
+║ .logic-eai
+║ .talkai
+
+╚═══════════════════════╝
+
+╔══〔 📖 SCRIPTURE AI 〕══╗
+
+║ .bible
+║ .quran
+
+╚═══════════════════════╝
+
+╔═══〔 🛠️ AI TOOLS 〕════╗
+
+║ .vision
+║ .analyse
+║ .totext
+║ .speechwrite
+║ .imagine
+║ .flux
+║ .magicstudio
+║ .genimage
+║ .aiart
+║ .aisong
+║ .makesong
+
+╚═══════════════════════╝
+`
+
+module.exports = aiMenu
