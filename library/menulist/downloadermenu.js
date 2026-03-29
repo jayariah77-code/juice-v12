a2c99453d3711fb385ee9eb033f1af8ece8436c5
jayariah77-code <jayariah77@gmail.com>
2026-03-29 23:33:07 +0300

    Add library/menulist/downloadermenu.js

---
+++ library/menulist/downloadermenu.js
@@ -0,0 +1,20 @@
+const downloaderMenu = `
+╔══〔 📥 MEDIA DOWNLOADERS 〕══╗
+
+║ .play
+║ .song
+║ .video
+║ .ytdocplay
+║ .ytdocvideo
+║ .ig
+║ .tt
+║ .fb
+║ .spotify
+║ .mediafire
+║ .apk
+║ .gitclone
+
+╚═══════════════════════╝
+`
+
+module.exports = downloaderMenu
