95b5390792023db4e3ec204ad17f201e7b22cc1d
jayariah77-code <jayariah77@gmail.com>
2026-03-29 23:33:12 +0300

    Add library/menulist/othermenu.js

---
+++ library/menulist/othermenu.js
@@ -0,0 +1,77 @@
+const otherMenu = `
+╔══〔 🎨 IMAGE EFFECTS 〕══╗
+
+║ .blur
+║ .sharpen
+║ .greyscale
+║ .negative
+║ .sepia
+║ .circle
+║ .jail
+║ .passed
+║ .triggered
+║ .comrade
+║ .lgbt
+║ .lolice
+║ .glass
+
+╚═══════════════════════╝
+
+╔══〔 ✨ TEXT EFFECTS 〕═══╗
+
+║ .metallic
+║ .ice
+║ .snow
+║ .neon
+║ .fire
+║ .glitch
+║ .thunder
+║ .matrix
+║ .hacker
+║ .devil
+║ .purple
+║ .blackpink
+║ .sand
+║ .arena
+║ .1917
+║ .light
+║ 💡 All commands accept [text]
+
+╚═══════════════════════╝
+
+╔══〔 🔤 FONT CONVERTER 〕══╗
+
+║ .fonts
+║ .allfonts
+║ .bold
+║ .italic
+║ .bolditalic
+║ .mono
+║ .serif
+║ .serifbold
+║ .serifitalic
+║ .scriptfont
+║ .scriptbold
+║ .fraktur
+║ .frakturbold
+║ .doublestruck
+║ .smallcaps
+║ .bubble
+║ .bubblebold
+║ .square
+║ .squarebold
+║ .wide
+║ .upsidedown
+║ .strikethrough
+║ .underline
+║ .currency
+║ .dotted
+║ .oldeng
+║ .aesthetic
+║ .cursive
+║ 💡 All commands accept [text]
+
+╚═══════════════════════╝
+`
+
+module.exports = otherMenu
