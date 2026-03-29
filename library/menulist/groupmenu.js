d76f1a12e1ed34178066e386d5ce246155ac9f02
jayariah77-code <jayariah77@gmail.com>
2026-03-29 23:33:11 +0300

    Add library/menulist/groupmenu.js

---
+++ library/menulist/groupmenu.js
@@ -0,0 +1,62 @@
+const groupMenu = `
+╔══〔 👥 MEMBER MANAGEMENT 〕══╗
+
+║ .add
+║ .kick
+║ .remove
+║ .promote
+║ .demote
+║ .ban
+║ .unban
+║ .warn
+║ .unwarn
+║ .clearwarn
+║ .warnlist
+║ .approve
+║ .reject
+║ .delete
+║ .mute
+║ .unmute
+║ .open
+║ .close
+║ .kickall
+
+╚═══════════════════════╝
+
+╔══〔 ⚙️ GROUP SETTINGS 〕══╗
+
+║ .setgname
+║ .setgdesc
+║ .setgpic
+║ .link
+║ .revoke
+║ .hidetag
+║ .tagall
+║ .creategroup
+║ .swgc
+║ .getsw
+║ .disp-1
+║ .disp-7
+║ .disp-90
+║ .disp-off
+
+╚═══════════════════════╝
+
+╔══〔 🛡️ GROUP PROTECTION 〕══╗
+
+║ .antilink
+║ .antilinkgc
+║ .antiimage
+║ .antivideo
+║ .antimention
+║ .antigroupstatus
+║ .antistatusmention →  on/warn/kick
+║ .antibot
+║ .antidelete
+║ .welcome
+║ .trash-group
+
+╚═══════════════════════╝
+`
+
+module.exports = groupMenu
