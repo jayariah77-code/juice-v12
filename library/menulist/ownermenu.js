47c97e6c0b83ea173e0882ce72fdb8563e5da911
jayariah77-code <jayariah77@gmail.com>
2026-03-29 23:33:13 +0300

    Add library/menulist/ownermenu.js

---
+++ library/menulist/ownermenu.js
@@ -0,0 +1,77 @@
+const ownerMenu = `
+  ╔══〔 👑 OWNER CONTROLS 〕══╗
+
+  ╠══〔 ⚙️ BOT SETTINGS 〕══╣
+  ║ .setbotname
+  ║ .author
+  ║ .packname
+  ║ .timezone
+  ║ .setprefix
+  ║ .botpic
+  ║ .boturl
+  ║ .setowner
+  ║ .pp
+  ║ .setpp
+  ║ .menuimage
+  ║ .addplugin
+  ║ .rmplugin
+  ║ .cgplugin
+  ║ .getplugin
+  ║ .botsettings
+
+  ╠═════〔 🔧 SYSTEM 〕═════╣
+  ║ .> / .$ / .=>
+  ║ .self
+  ║ .public
+  ║ .onlygroup
+  ║ .onlypc
+  ║ .unavailable
+  ║ .restart
+  ║ .join
+  ║ .leave
+  ║ .broadcast
+  ║ .sysinfo
+  ║ .cleartmp
+
+  ╠══〔 🛡️ SUDO MANAGEMENT 〕══╣
+  ║ .addsudo
+  ║ .delsudo
+  ║ .sudolist
+
+  ╠══〔 👻 PRESENCE & PRIVACY 〕══╣
+  ║ .alwaysonline
+  ║ .lastseen
+  ║ .idch
+  ║ .autotyping
+  ║ .autorecording
+  ║ .autoonline
+
+  ╠═══〔 🚫 PROTECTION 〕═══╣
+  ║ .anticall
+  ║ .pmblocker
+  ║ .block
+  ║ .unblock
+  ║ .listblock
+
+  ╠═══〔 🔄 AUTOMATION 〕═══╣
+  ║ .autoread
+  ║ .chatbot
+  ║ .autobio
+  ║ .autoreact
+  ║ .autoreplystatus
+  ║ .autoviewstatus
+  ║ .autolikestatus
+  ║ .muslimprayer
+  ║ .christianprayer
+
+  ╠══〔 🤖 AI CHATBOT MODES 〕══╣
+  ║ .setaimode status
+  ║ .setaimode global
+  ║ .setaimode dm
+  ║ .setaimode group
+  ║ .setaimode reset
+
+  ╚═══════════════════════╝
+  `
+
+  module.exports = ownerMenu
