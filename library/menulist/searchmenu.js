31850aeff9c71b463fbf114c0eeda1bf27f8b9e7
jayariah77-code <jayariah77@gmail.com>
2026-03-29 23:33:13 +0300

    Add library/menulist/searchmenu.js

---
+++ library/menulist/searchmenu.js
@@ -0,0 +1,44 @@
+const searchMenu = `
+  ╔═══〔 🔍 WEB SEARCH 〕═══╗
+
+  ║ .google
+  ║ .define
+  ║ .dictionary
+  ║ .playstore
+  ║ .yts
+  ║ .img
+
+  ╚═══════════════════════╝
+
+  ╔══〔 🎬 MEDIA SEARCH 〕══╗
+
+  ║ .movie
+  ║ .stream
+  ║ .anime
+  ║ .manga
+  ║ .wallpaper
+  ║ .lyrics
+
+  ╚═══════════════════════╝
+
+  ╔══〔 📰 NEWS & INFO 〕═══╗
+
+  ║ .news
+  ║ .wiki
+  ║ .urban
+  ║ .recipe
+  ║ .food
+
+  ╚═══════════════════════╝
+
+  ╔═════〔 🐙 GITHUB 〕═════╗
+
+  ║ .github
+  ║ .git
+  ║ .repo
+  ║ .clone
+
+  ╚═══════════════════════╝
+  `
+
+  module.exports = searchMenu
