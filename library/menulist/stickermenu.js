a41dd91cb8b2fbfd6df91d59688fd2a4bd09157a
jayariah77-code <jayariah77@gmail.com>
2026-03-29 23:33:14 +0300

    Add library/menulist/stickermenu.js

---
+++ library/menulist/stickermenu.js
@@ -0,0 +1,29 @@
+const stickerMenu = `
+  ╔══〔 🖼️ STICKER MAKER 〕══╗
+
+  ║ .sticker
+  ║ .stickercrop
+  ║ .scrop
+  ║ .take
+  ║ .steal
+  ║ .qc
+  ║ .brat
+  ║ .attp
+  ║ .ttp
+  ║ .bratv
+  ║ .emojimix
+  ║ .telestick
+  ║ .stikerly
+  ║ .smeme
+
+  ╚═══════════════════════╝
+
+  ╔══〔 🔄 MEDIA CONVERTER 〕══╗
+
+  ║ .toimage
+  ║ .tourl
+
+  ╚═══════════════════════╝
+  `
+
+  module.exports = stickerMenu
